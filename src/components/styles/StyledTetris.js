7e8336be83c28772dd96227a586c595622289316
JamJuiceCreative <jim@jamjuicecreative.com.au>
2023-10-06 12:21:50 +1000

    finished coding tetris game

--- src/components/styles/StyledTetris.js
+++ src/components/styles/StyledTetris.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-
+// BG Image
 import bgImage from '../../img/bg.png';
 
 export const StyledTetrisWrapper = styled.div`
@@ -11,19 +11,16 @@ export const StyledTetrisWrapper = styled.div`
 `;
 
 export const StyledTetris = styled.div`
-display: flex;
-align-items: flex-start;
-padding: 40px;
-margin: 0 auto;
-max-width: 900px;
+  display: flex;
+  align-items: flex-start;
+  padding: 40px;
+  margin: 0 auto;
+  max-width: 900px;
 
-
-aside {
+  aside {
     width: 100%;
     max-width: 200px;
     display: block;
     padding: 0 20px;
-    
-}
-`
-
+  }
+`;
